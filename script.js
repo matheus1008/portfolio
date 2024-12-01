dd9ef7e961eab1ea2ad65d956387761b921c656e
matheus1008 <137730086+matheus1008@users.noreply.github.com>
2024-12-01 03:21:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -11,33 +11,20 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-function showSlides() {
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
   let captionText = document.getElementById("caption");
-
-  // Reset slides
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-
-  // Reset dots
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-
-  // Adjust slideIndex
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-
-  // Display the current slide and dot
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
-
-  // Set timeout for next slide
-  setTimeout(showSlides, 3000); // 3000ms = 3 seconds
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
